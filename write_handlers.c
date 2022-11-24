b05824f179d21f58578836322d5e5113631feba6
Noahire <noahire@gmail.com>
2022-11-24 20:25:15 +0000

    more functions

---
+++ write_handlers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/************************* WRITE HANDLE *************************/
+/**
+ * handle_write_char - Prints a string
+ * @c: char types.
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: precision specifier
+ * @size: Size specifier
+ *
+ * Return: Number of chars printed.
+ */
+
